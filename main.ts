8c89903a0360b1ec7b0ccb2be75ca1d138f3267e
Valesz09 <v.espana@uniandes.edu.co>
2026-04-13 00:01:04 -0500

    TALLER 2 MODIFICACION

--- main.ts
+++ main.ts
@@ -2,6 +2,7 @@ import { series } from "./data";
 
 let tableBody: HTMLElement = document.getElementById("series-table")!;
 let avgElement: HTMLElement = document.getElementById("average")!;
+let detailElement: HTMLElement = document.getElementById("serie-detail")!;
 
 function renderSeries() {
     let totalSeasons = 0;
@@ -16,12 +17,30 @@ function renderSeries() {
             <td>${serie.seasons}</td>
         `;
 
+
+        row.addEventListener("click", () => showDetail(serie));
+
         tableBody.appendChild(row);
         totalSeasons += serie.seasons;
     });
 
     let average = totalSeasons / series.length;
     avgElement.innerHTML = `<strong>Seasons average:</strong> ${average.toFixed(2)}`;
+}
+
+function showDetail(serie: any) {
+    detailElement.innerHTML = `
+        <div class="card">
+            <img class="card-img-top" src="${serie.image}" alt="${serie.name}">
+            <div class="card-body">
+                <h5 class="card-title">${serie.name}</h5>
+                <p class="card-text">${serie.description}</p>
+                <a href="${serie.link}" target="_blank" class="btn btn-primary">
+                    Go to website
+                </a>
+            </div>
+        </div>
+    `;
 }
 
 renderSeries();
